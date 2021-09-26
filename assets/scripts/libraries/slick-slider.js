3f5b39540845e032b86ce772da27ed6894b9bcd8
Navaraj Subedi <srizonsansar@gmail.com>
2021-09-26 23:09:17 +0100

    Foundation Section Done

--- assets/scripts/libraries/slick-slider.js
+++ assets/scripts/libraries/slick-slider.js
@@ -3,5 +3,18 @@ $(document).ready(function(){
     $('.foundation__slides').slick({
         dots: true,
         arrows: false,
+        mobileFirst: true,
+        responsive: [
+            {
+              breakpoint: 759,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+              breakpoint: 999,
+              settings: 'unslick'
+            }
+          ]
     })
 })
