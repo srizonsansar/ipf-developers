42beece04b65b1a994eb10c2147080124f8a2dd7
Navaraj Subedi <srizonsansar@gmail.com>
2021-10-01 15:26:48 +0100

    Mobile Menu Level 1 Done

--- assets/scripts/common/navigation.js
+++ assets/scripts/common/navigation.js
@@ -4,11 +4,28 @@ $(document).ready(function(){
     $('.header__midRow__navTrigger').on('click', function(){
         $(this).children('.line2').toggleClass('hidden')
         $(this).children('.line1, .line3').toggleClass('active')
-        $('.sideNav').toggleClass('active')
+        $('.navigation').toggleClass('active')
         $('body').toggleClass('OFHidden')
         // Hide Search Bar in Mobile
         $('.header__searchBar').slideUp();
         $('.header').removeClass('shadow')
+        $('.navigation__mainNav').addClass('active')
     })
+
+    // Close Menu on Overlay Click
+    $('.navigation').on('click', function(){
+        var navLines = $('.header__midRow__navTrigger .line1, .header__midRow__navTrigger .line3')
+        var navHiddenLine = $('.header__midRow__navTrigger .line2')
+        $(this).removeClass('active')
+        $('body').removeClass('OFHidden')
+        navLines.removeClass('active')
+        navHiddenLine.removeClass('hidden')
+        $('.navigation__mainNav').removeClass('active')
+    })
+
+    // Disable Parent Div click function in Nav Div
+    $('.navigation__mainNav').on('click', function(e) {
+        e.stopPropagation();
+     });
 
 })
