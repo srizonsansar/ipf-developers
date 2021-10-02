a074fad62896650db0deef59ceebfd5c1e1e8961
Navaraj Subedi <srizonsansar@gmail.com>
2021-10-02 09:57:26 +0100

    Small issues on mobile nav and fixed

--- assets/scripts/common/navigation.js
+++ assets/scripts/common/navigation.js
@@ -4,59 +4,51 @@ $(document).ready(function(){
     $('.header__midRow__navTrigger').on('click', function(){
         $(this).children('.line2').toggleClass('hidden')
         $(this).children('.line1, .line3').toggleClass('active')
+        $('.navigation-overlay').toggleClass('active')
         $('.navigation').toggleClass('active')
         $('body').toggleClass('OFHidden')
         // Hide Search Bar in Mobile
         $('.header__searchBar').slideUp();
         $('.header').removeClass('shadow')
-        $('.navigation__mainNav').toggleClass('active')
     })
 
-    // Close Menu on Overlay Click
-    $('.navigation').on('click', function(){
-        var navLines = $('.header__midRow__navTrigger .line1, .header__midRow__navTrigger .line3')
-        var navHiddenLine = $('.header__midRow__navTrigger .line2')
+    $('.navigation-overlay').on('click', function(){
         $(this).removeClass('active')
+        $('.navigation').removeClass('active')
+        $('.header').find('.line2').removeClass('hidden')
+        $('.header').find('.line1, .line3').removeClass('active')
         $('body').removeClass('OFHidden')
-        navLines.removeClass('active')
-        navHiddenLine.removeClass('hidden')
-        $('.navigation__mainNav').removeClass('active')
     })
-
-    // Disable Parent Div click function in Nav Div
-    $('.navigation__mainNav').on('click', function(e) {
-        e.stopPropagation();
-    });
 
     // Go to Level 2
-    $('.navigation__mainNav__nav--item.open-level2menu').on('click', function(){
+    $('.navigation__nav--item.open-level2menu').on('click', function(){
         var targetMenu = $(this).data('menu')
-        $(this).parents('.navigation__mainNav--navBlock').hide()
-        $('.navigation__mainNav--navBlock.' + targetMenu).fadeIn()
-        $('.navigation__mainNav__newsBlock').fadeIn()
+        $(this).parents('.navigation--navBlock').hide()
+        $('.navigation--navBlock.' + targetMenu).fadeIn()
+        $('.navigation__newsBlock').fadeIn()
     })
 
     // Go to Level 3
-    $('.navigation__mainNav__nav--item.open-level3menu').on('click', function(){
+    $('.navigation__nav--item.open-level3menu').on('click', function(){
         var targetMenu = $(this).data('menu')
         var parentMenu = $(this).data('parent')
-        $('.navigation__mainNav--navBlock.' + parentMenu).hide()
-        $('.navigation__mainNav--navBlock.'+ targetMenu).fadeIn()
+        $('.navigation--navBlock.' + parentMenu).hide()
+        $('.navigation--navBlock.'+ targetMenu).fadeIn()
     })
 
     // Back to Level 1
-    $('.navigation__mainNav__nav--item.level2parent').on('click', function(){
-        $(this).parents('.navigation__mainNav--navBlock.level2menu').hide()
-        $('.navigation__mainNav--navBlock.level1Menu').fadeIn()
-        $('.navigation__mainNav__newsBlock').hide()
+    $('.navigation__nav--item.level2parent').on('click', function(){
+        $(this).parents('.navigation--navBlock.level2menu').hide()
+        $('.navigation--navBlock.level1Menu').fadeIn()
+        $('.navigation__newsBlock').hide()
     })
 
     // Back to Level 2
-    $('.navigation__mainNav__nav--item.level3parent').on('click', function(){
+    $('.navigation__nav--item.level3parent').on('click', function(){
         var currentMenu = $(this).data('menu')
         var parentMenu = $(this).data('parent')
-        $('.navigation__mainNav--navBlock.' + parentMenu).fadeIn()
-        $('.navigation__mainNav--navBlock.' + currentMenu).hide()
+        $('.navigation--navBlock.' + parentMenu).fadeIn()
+        $('.navigation--navBlock.' + currentMenu).hide()
     })
 
     // Desktop Nav
